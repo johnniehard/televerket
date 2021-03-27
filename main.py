b37c354e11d046e02592f7e25330e68bd43e3cfc
Johnnie Hard <johnnie@synvinkel.org>
2021-03-27 07:02:27 +0000

    make call request

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 from gpiozero import Button
 import requests
+import os
+
+state = 'IDLE'
+
+auth = (os.environ['CALL_API_USERNAME'], os.environ['CALL_API_PASSWORD'])
+
+fields = {
+        'from': os.environ['CALLER_NUMBER'],
+        'to': os.environ['TARGET_NUMBER'],
+        'voice_start': '{}'
+    }
+
+print(fields)
+print(auth)
 
 def button_pressed():
-    print("PRESSED!")
+    print('PRESSED')
+    state = 'CALLING'
+    response = requests.post("https://api.46elks.com/a1/calls", data=fields, auth=auth)
+    print(response.content)
+    state = 'IDLE'
+
 
 button = Button(21)
 button.when_pressed = button_pressed
 
 while True:
-   button.wait_for_press() 
+   if state == 'IDLE':
+       button.wait_for_press() 
